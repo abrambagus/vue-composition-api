227c1789af2db46db150f7a03ab03254d129391a
abram <abrambagus@gmail.com>
2025-05-13 11:15:01 +0700

    add watcher and templater refs

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,22 @@
     <h3>{{ age }}</h3>
     <button @click="setAge">Change Age</button>
     <div>
-      <input type="text" placeholder="First Time" @input="setFirstName" />
-      <input type="text" placeholder="Last Time" @input="setLastName" />
+      <input type="text" placeholder="First Time" v-model="firstName" />
+      <input type="text" placeholder="Last Time" ref="lastNameInput" />
+      <button @click="setLastName">Set Name</button>
     </div>
   </section>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
   setup() {
     // const uName = ref('Abram');
     const firstName = ref('');
     const lastName = ref('');
+    const lastNameInput = ref(null);
     const uAge = ref(25);
 
     // const user = reactive({
@@ -29,23 +31,35 @@ export default {
       return firstName.value + ' ' + lastName.value;
     });
 
+    watch([uAge, uName], function (newValues, oldValues) {
+      console.log('old age: ', oldValues[0]);
+      console.log('new age: ', newValues[0]);
+      console.log('old name: ', oldValues[1]);
+      console.log('new name: ', newValues[0]);
+    });
+
     function setNewAge() {
       uAge.value = 26;
     }
 
-    function setFirstName(event) {
-      firstName.value = event.target.value;
-    }
+    // function setFirstName(event) {
+    //   firstName.value = event.target.value;
+    // }
 
-    function setLastName(event) {
-      lastName.value = event.target.value;
+    // function setLastName(event) {
+    //   lastName.value = event.target.value;
+    // }
+
+    function setLastName() {
+      lastName.value = lastNameInput.value.value;
     }
 
     return {
       userName: uName,
       age: uAge,
       setAge: setNewAge,
-      setFirstName,
+      firstName,
+      lastNameInput,
       setLastName,
     };
   },
